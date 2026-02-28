05deaf73472140a1cdc4f5325bf8a74fdfe820b1
Kener-Cartagena <kenerdataanalyst@gmail.com>
2026-02-28 12:44:40 -0600

    Fix API_URL trailing slash in api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const API_URL_LOCAL = 'http://localhost:8000/api';
-const API_URL = 'https://taskflow-backend-9n9s.onrender.com';
+const API_URL = 'https://taskflow-backend-9n9s.onrender.com/';
 
 class API {
     static async request(endpoint, options = {}) {
